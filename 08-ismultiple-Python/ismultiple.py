310a22aec9ebfdf2e1048b318c313c50a608bc03
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-09 21:58:42 +0530

    ismultiple

--- 08-ismultiple-Python/ismultiple.py
+++ 08-ismultiple-Python/ismultiple.py
@@ -6,4 +6,10 @@
 
 
 def fun_ismultiple(m, n):
-	return False # replace with your solution
+	if n == 0 and m == 0:
+    		return True 
+	if n == 0 or m < 0:
+    		return False
+	if m % n == 0:
+		return True
+	return False
