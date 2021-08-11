334092edf9c5a614f8dd505706ba245fdb9bbc0e
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-11 22:33:25 +0530

    Done

--- 12-playStep2-Python/playstep2.py
+++ 12-playStep2-Python/playstep2.py
@@ -32,24 +32,46 @@
 # Hint: Then, you may wish to use diceToOrderedHand(a, b, c) at the end to convert the 3 dice back
 # into a sorted hand.
 # Hint: Also, remember to use % to get the one's digit, and use //= to get rid of the one's digit.
+
+def dicetoorderedhand(a, b, c):
+    	# your code goes here
+	val = (a,b,c)
+	mx = max(val)
+	mn = min(val)
+	left = a + b + c - mn - mx
+	return mx*10**(2)+left*10+mn
+
+def handtodice(hand):
+    	# your code goes here
+	hand = str(hand)
+	val = list()
+	for i in hand:
+    		val.append(int(i))
+	return tuple(val)
+
+def checkmatch(hand):
+    d1 = hand[0]
+    d2 = hand[1]
+    d3 = hand[2]
+    if d1 == d2:
+        return d1,d2
+    elif d2 == d3:
+        return d2,d3
+    elif d1 == d3:
+        return d1,d3
+
 def playstep2(hand, dice):
-    hand = str(hand)
-    dice = str(dice)
-    check = set(hand)
-    if len(check)== len(hand):
-        out = str(hand[0])
-        for i in range (1,len(hand)):
-            out += dice[-1:]
-            dice = dice[:len(dice)-1]
-        out = sorted(out,reverse=True)
-        out = "".join(out)
-        return int(out),int(dice)
-    else:
-        out = ""
-        for i in range (1,len(hand)):
-            out += hand[i]
-        out = dice[-1:]+out
-        out = sorted(out,reverse=True)
-        out = "".join(out)
-        dice = dice[:len(dice)-1]
-        return int(out),int(dice)
+    hand = handtodice(hand)
+    if checkmatch(hand):
+        hand = checkmatch(hand)
+        future = dice%10
+        dice = dice//10
+        hand = dicetoorderedhand(hand[0],hand[1],future)
+        return hand,dice
+    hand = max(hand)
+    future = dice%100
+    dice = dice // 100
+    hand = future * 10 + hand
+    hand = handtodice(hand)
+    return dicetoorderedhand(hand[0],hand[1],hand[2]), dice
+print(playstep2(413, 2312))
