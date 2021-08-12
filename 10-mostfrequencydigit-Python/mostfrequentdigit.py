b9391d291b4431cfc5c996efcdd8b11f1bbb7b55
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-12 20:29:55 +0530

    Done

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -4,4 +4,32 @@
 
 def mostfrequentdigit(n):
 	# your code goes here
-	pass
+	x = n
+	out = 0
+	output = 0
+	while True:
+		if x == 0:
+			break
+		i = x%10
+		x = x//10
+		c = count(n,i)
+		if c > out :
+			out = c
+			output = i
+		elif c == out:
+			if output > i:
+				output = i 
+	return output
+
+def count(n,val):
+	num = 0
+	while True:
+		if n == 0:
+			break
+		i = n%10
+		n = n//10
+		if val == i:
+			num += 1
+	return num
+
+print(mostfrequentdigit(0))
