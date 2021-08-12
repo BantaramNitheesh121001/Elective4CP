511bce0f4255189ea4926548bb78b19601eff624
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-12 20:47:21 +0530

    Done

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,4 +6,8 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	area = (x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2))
+	if area % 2 > 0:
+		return area/2
+	return int(area/2)
+print(triangleareabycoordinates(23, 30, 15, 15, 50, 25))
