d24006b89e9a141fba69ba441fe0702c9797cd27
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-13 13:29:39 +0530

    Done

--- 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
+++ 09-hasconsecutivedigits-Python/hasconsecutivedigits.py
@@ -4,4 +4,15 @@
 
 def hasconsecutivedigits(n):
 	# your code goes here
-	pass
+	n = abs(n)
+	if n < 10:
+		return False
+	while(True):
+		if n < 9:
+			break
+		val = n%10
+		n = n//10
+		val1 = n%10
+		if val == val1 + 1 or val == val1-1:
+			return True
+	return False
