36d41289a44db0d9d00cddfee676d96de1306f40
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-15 16:16:39 +0530

    Done

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,25 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = distance(x1,y1,x2,y2)
+	b = distance(x2,y2,x3,y3)
+	c = distance(x1,y1,x3,y3)
+	h = max(a,b,c)
+	s = min(a,b,c)
+	o = a+b+c-h-s
+	h = h**2
+	s = s**2
+	o = o**2
+	if almostEqual(h,s+o):
+		return True
+	return False
+
+def distance(x1, y1, x2, y2):
+	# your code goes here
+	X = (x2 - x1)**2
+	Y = (y2 - y1)**2
+	d = (X+Y)**(1/2)
+	return d
+
+def almostEqual(x, y):
+	return abs(x - y) < 10**-9
