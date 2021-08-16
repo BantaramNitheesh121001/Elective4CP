b533ece90ce98d501b912125a7d4ce1c6791164a
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-16 11:23:34 +0530

    Done

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -6,4 +6,19 @@
 
 def smallestdifference(a):
 	# Your code goes here
-	pass
+	small = 0
+	if len(a) == 0:
+		return -1
+	for i in range (len(a)):
+		if i+1 == len(a):
+			break
+		for j in range (i+1,len(a)):
+			val = abs(a[i] - a[j])
+			if i == 0 and j == 1:
+				small = val
+			if val <= small:
+				small = val
+	return small
+
+a = [19,2,83,6,27]
+print(smallestdifference(a))
