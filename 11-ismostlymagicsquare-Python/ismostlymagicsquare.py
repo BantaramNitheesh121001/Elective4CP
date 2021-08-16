8753bd339f8fb264a9d62d799792555b4ff4d3a9
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-16 14:23:37 +0530

    Done

--- 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
+++ 11-ismostlymagicsquare-Python/ismostlymagicsquare.py
@@ -13,6 +13,48 @@
 #   [ 2, 1]]
 # Each row and each column add to 3, but one diagonal adds to 2 and the other to 4.
 
+def add(a):
+	val = 0
+	for i in a:
+		val += i
+	return val
+
+def diagonal1(a):
+	val = 0
+	for i in range(len(a)):
+		for j in range (len(a)):
+			if i == j:
+				val += a[i][j]
+	return int(val)
+
+def diagonal2(a):
+	val = 0
+	n = len(a)
+	for i in range(n):
+		for j in range (n):
+			if i+j == n-1:
+				val += a[i][j]
+	return int(val)
+
 def ismostlymagicsquare(a):
 	# Your code goes here
-	pass
+	check = int(add(a[0]))
+	for i in a:
+		print(add(i))
+		if check != add(i):
+			return False
+
+	for i in range (len(a)):
+		value = 0
+		for j in range (len(a[i])):
+			value += a[j][i]
+		print (value)
+		if value != check:
+			return False
+	print (diagonal1(a),diagonal2(a))
+	if check == diagonal1(a) and check == diagonal2(a):
+		return True
+	return False
+
+a = [[16, 3, 2, 13], [5, 10, 11, 8], [9, 6, 7, 12],[4, 15, 14, 1]]
+print (ismostlymagicsquare(a))
