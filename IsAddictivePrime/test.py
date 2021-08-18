a320c46ebfcdc57b591f837c87b8447270183a40
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-18 12:21:45 +0530

    Done

---
+++ IsAddictivePrime/test.py
@@ -0,0 +1,16 @@
+import os,sys
+sys.path.append(os.getcwd())
+from IsAddictivePrime import IsAdditivePrime
+import pytest
+
+
+@pytest.mark.parametrize('m, result',[
+	(2, True), (3, True),
+	(5, True), (13, False),
+	(23, True), (29, True),
+	(41, True), (98, False),
+	(290, True), (198, False),
+	(67, True), (97, False)
+])
+def test(m, result):
+    assert IsAdditivePrime(m) == result
