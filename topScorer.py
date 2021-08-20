995f7084a9253ffacec871cd2b1223c46c37b33a
BantaramNitheesh121001 <nitheeshbantaram1@msitprogram.net>
2021-08-20 21:34:06 +0530

    Done

--- topScorer.py
+++ topScorer.py
@@ -16,24 +16,53 @@
 
 def topScorer(data):
     # Your code goes here...
-    return ""
+    if data == "":
+        return None
+    l = []
+    data = data.splitlines()
+    for i in range (len(data)):
+        data[i] = data[i].split(",")
+    for i in range (len(data)):
+        val = 0
+        for j in range (len(data[i])):
+            if j == 0:
+                l.append(data[i][j])
+            else:
+                val += int(data[i][j])
+        l.append(val)
+    i = 1
+    val = 0
+    output = ''
+    while True:
+        if l[i] > val:
+            val = l[i]
+            output = l[i-1]
+        elif l[i] == val:
+            output += ","+ l[i-1]
+        i += 2
+        if i > len(l):
+            break
+    return output
 
 data = '''\
 Fred,10,20,30,40
 Wilma,10,20,30
 '''
+print(topScorer(data))
 assert(topScorer(data) == 'Fred')
 
 data = '''\
 Fred,10,20,30
 Wilma,10,20,30,40
 '''
+print(topScorer(data))
 assert(topScorer(data) == 'Wilma')
 
 data = '''\
 Fred,11,20,30
 Wilma,10,20,30,1
 '''
+print(topScorer(data))
 assert(topScorer(data) == 'Fred,Wilma')
 
 assert(topScorer('') == None)
